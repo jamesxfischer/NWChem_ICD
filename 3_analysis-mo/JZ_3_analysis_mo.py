78b13695a64c69e5d7fc763d985490c96699ebe6
James Zhong <xzhong34@gatech.edu>
2023-02-08 16:58:25 -0500

    Delete and update files because of bugs

--- 3_analysis-mo/JZ_3_analysis_mo.py
+++ 3_analysis-mo/JZ_3_analysis_mo.py
@@ -1,5 +1,5 @@
 ################################################################
-JZ 1/12/2023 wrapper to call all .py files
+##JZ 1/12/2023 wrapper to call all .py files
 ################################################################
 import os
 import datetime
@@ -7,21 +7,16 @@ ct = datetime.datetime.now()
 ts = str(ct)
 
 #call nw gen function to create .nw files
-print(ts + ' Start -- generate nw files')
+print(ts + ' Start -- 1_charge.py')
 os.system('python3 1_charge.py')
-print(ts + ' Done -- generate nw files')
+print(ts + ' Done -- 1_charge.py')
 
 #generate slm files
-print(ts + ' Start -- generate slm files')
+print(ts + ' Start -- 1_charge_plot.py')
 os.system('python3 1_charge_plot.py')
-print(ts + ' Done -- generate slm files')
+print(ts + ' Done -- 1_charge_plot.py')
 
 #submit nw files with slm
-print(ts + ' Start -- submit slm files')
-os.system('python3 1_NWrtparse_NO_header.py')
-print(ts + ' Start -- submit slm files')
-
-#submit nw files with slm
-print(ts + ' Start -- submit slm files')
+print(ts + ' Start -- 2_moocc_os.py')
 os.system('python3 2_moocc_os.py')
-print(ts + ' Start -- submit slm files')
+print(ts + ' Start -- 2_moocc_os.py')
